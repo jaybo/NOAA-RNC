6413030f62f336b112e74db79378166527653ad8
Jay Borseth <jayborseth@gmail.com>
2020-03-04 11:58:05 -0800

    Allow flipped Y axis, add NamedTempoaryFile close on Windows

--- mbtilesQuilt.py
+++ mbtilesQuilt.py
@@ -11,42 +11,57 @@ import argparse
 from tempfile import NamedTemporaryFile
 from PIL import Image
 
+
 def mergeImage(ofn, png, qVerbose):
-  if qVerbose:
-    print('Merging', ofn)
-  with NamedTemporaryFile() as ifp:
-    ifp.write(png)
-    ifp.flush()
-    im0 = Image.open(ofn).convert('RGBA')
-    im1 = Image.open(ifp.name).convert('RGBA')
-    im2 = Image.alpha_composite(im0, im1).convert('P')
-    im2.save(ofn)
+    if qVerbose:
+        print('Merging', ofn)
+    with NamedTemporaryFile(delete=False) as ifp:
+        ifp.write(png)
+        ifp.flush()
+        ifp.close()
+        im0 = Image.open(ofn).convert('RGBA')
+        im1 = Image.open(ifp.name).convert('RGBA')
+        im2 = Image.alpha_composite(im0, im1).convert('P')
+        im2.save(ofn)
+        ifp.close()
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('mtiles', nargs='+', help='MTiles files to process')
 verbose = parser.add_mutually_exclusive_group()
-verbose.add_argument('--verbose', action='store_true', help='Output diagnositcs')
-verbose.add_argument('--quiet', action='store_true', help='No non-error output')
-parser.add_argument('--outdir', default='RNC_ROOT', help='where to write output to')
+verbose.add_argument('--verbose', action='store_true',
+                     help='Output diagnositcs')
+verbose.add_argument('--quiet', action='store_true',
+                     help='No non-error output')
+parser.add_argument('--outdir', default='RNC_ROOT',
+                    help='where to write output to')
+parser.add_argument('--flip_y', default=True,
+                    help='Flip Y axis for non-TMS servers')
+
 args = parser.parse_args()
 
 for fn in args.mtiles:
-  if not args.quiet:
-    print('Opening', fn)
-  with sqlite3.connect(fn) as conn:
-    results = conn.execute('SELECT * FROM tiles;')
-    for result in results: # Walk over rows
-      (zoom, column, row, png) = result;
-      odir = os.path.join(args.outdir, str(zoom), str(row))
-      ofn = os.path.join(odir, '{}.png'.format(column))
-      if not os.path.isdir(odir):
-        if args.verbose:
-          print('Making directory', odir)
-        os.makedirs(odir)
-      if os.path.exists(ofn):
-        mergeImage(ofn, png, args.verbose)
-      else: # Does not exist
-        if args.verbose:
-          print('Saving', ofn, 'length', len(png))
-        with open(ofn, 'wb') as ofp:
-          ofp.write(png)
+    if not args.quiet:
+        print('Opening', fn)
+    with sqlite3.connect(fn) as conn:
+        results = conn.execute('SELECT * FROM tiles;')
+        for result in results:  # Walk over rows
+            (zoom, column, row, png) = result
+            # if zoom > 7:
+            #   continue
+            # jayb Y is inverted in TMS (default format for MBTiles)
+            if args.flip_y:
+                row = (2 ** zoom) - (1 + row)
+            odir = os.path.join(args.outdir, 'Z' + str(zoom), str(row))
+            ofn = os.path.join(odir, '{}.png'.format(column))
+            if not os.path.isdir(odir):
+                # if args.verbose:
+                #   print('Making directory', odir)
+                os.makedirs(odir)
+            if os.path.exists(ofn):
+                mergeImage(ofn, png, args.verbose)
+            else:  # Does not exist
+                if args.verbose:
+                    print('Saving', ofn, 'length', len(png))
+                with open(ofn, 'wb') as ofp:
+                    ofp.write(png)
